0cb4ad2e3eea11beac8e2164be55efd20171f7df
Ben Wheatley <BenWheatley@users.noreply.github.com>
2017-11-17 13:54:33 +0100

    Add files via upload

---
+++ citation.py
@@ -0,0 +1,17 @@
+#!python
+
+import re
+
+source = r"Norman P. Jouppi, Cliff Young, Nishant Patil, David Patterson, Gaurav Agrawal, Raminder Bajwa, Sarah Bates, Suresh Bhatia, Nan Boden, Al Borchers, Rick Boyle, Pierre-luc Cantin, Clifford Chao, Chris Clark, Jeremy Coriell, Mike Daley, Matt Dau, Jeffrey Dean, Ben Gelb, Tara Vazir Ghaemmaghami, Rajendra Gottipati, William Gulland, Robert Hagmann, C. Richard Ho, Doug Hogberg, John Hu, Robert Hundt, Dan Hurt, Julian Ibarz, Aaron Jaffey, Alek Jaworski, Alexander Kaplan, Harshit Khaitan, Andy Koch, Naveen Kumar, Steve Lacy, James Laudon, James Law, Diemthu Le, Chris Leary, Zhuyuan Liu, Kyle Lucke, Alan Lundin, Gordon MacKean, Adriana Maggiore, Maire Mahony, Kieran Miller, Rahul Nagarajan, Ravi Narayanaswami, Ray Ni, Kathy Nix, Thomas Norrie, Mark Omernick, Narayana Penukonda, Andy Phelps, Jonathan Ross, Matt Ross, Amir Salek, Emad Samadiani, Chris Severn, Gregory Sizikov, Matthew Snelham, Jed Souter, Dan Steinberg, Andy Swing, Mercedes Tan, Gregory Thorson, Bo Tian, Horia Toma, Erick Tuttle, Vijay Vasudevan, Richard Walter, Walter Wang, Eric Wilcox, Doe Hyun Yoon"
+
+find = r"([^,]*) ([^,]*)(, |$)"
+replace = r" | last1 = \2 | first1 = \1\n"
+
+matches = re.findall(find, source)
+
+for matchIndex in range(matches.__len__()):
+	match = matches[matchIndex]
+	i = str(matchIndex)
+	lastName = match[1]
+	firstName = match[0]
+	print("\t| last"+i+" = "+lastName+" | first"+i+" = "+firstName)
